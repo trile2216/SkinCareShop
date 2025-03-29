2d6c47fa661cf1c04402394b5e8a2b3d226664ec
AnLeTruc <letrucan096666@gmail.com>
2025-03-29 15:38:20 +0700

    Update search

--- FE/FrontEnd/src/components/Header.jsx
+++ FE/FrontEnd/src/components/Header.jsx
@@ -1,25 +1,28 @@
-import { useState } from "react";
+import { useState,  } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FiSearch, FiShoppingCart, FiUser } from "react-icons/fi";
 import Cart from "./Cart";
-import { useDispatch, useSelector } from "react-redux";
 import { useCart } from "../context/CartContext";
-import { logout } from "../context/UserSlice";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useAuth from "../context/useAuth";
+import { useSearch } from "../context/SearchContext";
 
 const Header = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const { cartCount } = useCart();
   const [showLogoutConfirm, setShowLogoutConfirm] = useState(false);
-
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const token = localStorage.getItem("token");
   const isLoggedIn = !!token;
   const { logoutUser } = useAuth();
+  const { searchQuery, setSearchQuery } = useSearch(); 
+
+  // Search 
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value); 
+  };
 
   const handleLogout = () => {
     logoutUser();
@@ -64,8 +67,10 @@ const Header = () => {
             <div className="relative w-2/5">
               <input
                 type="text"
-                placeholder="Search for products..."
+                placeholder="Search by name"
                 className="w-full px-5 py-2 border text-sm border-gray-200 rounded-full focus:outline-none focus:ring-2 focus:ring-rose-300 focus:border-transparent transition-all duration-300"
+                value={searchQuery}
+                onChange={handleSearchChange} 
               />
               <button className="absolute right-3 top-2.5 text-gray-400 hover:text-rose-500 transition-colors duration-300">
                 <FiSearch size={20} />
