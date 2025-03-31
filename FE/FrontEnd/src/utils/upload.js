a71b5c584aa03a8b9501a4e6ac8ccb11c9314ff0
mintu1801 <thultmse185044@fpt.edu.vn>
2025-04-01 04:44:54 +0700

    update upload.js

--- FE/FrontEnd/src/utils/upload.js
+++ FE/FrontEnd/src/utils/upload.js
@@ -1,10 +1,36 @@
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../config/firebase";
 
-const uploadFile = async (file) => {
+export const uploadFile = async (file) => {
     const storageRef = ref(storage, file.name);
     const response = await uploadBytes(storageRef, file);
     const downloadUrl = await getDownloadURL(response.ref);
     return downloadUrl;
 };
-export default uploadFile;
+
+
+export const uploadFileCloudinary = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "SmartRoom");
+  
+      const response = await fetch(
+        "https://api.cloudinary.com/v1_1/dgzsjtvo5/image/upload",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to upload image to Cloudinary");
+      }
+  
+      const data = await response.json();
+      return data.secure_url;
+    } catch (error) {
+      console.error("Error uploading to Cloudinary:", error);
+      throw error;
+    }
+  };
